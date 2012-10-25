2d119b087fe68118fea0fe6746d194409f2c3a79
Toby Joe Boudreaux <tj@tobyjoe.com>
2012-10-25 16:05:31 -0400

    Moved Beacon to sep class

--- BeaconPCAP/src/BeaconPCAPApp.cpp
+++ BeaconPCAP/src/BeaconPCAPApp.cpp
@@ -9,14 +9,7 @@
 
 #include "Resources.h"
 #include "Constants.h"
-
-#include <pcap/pcap.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/if_ether.h> /* includes net/ethernet.h */
-#include "cinder/Thread.h"
+#include "Beacon.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -31,125 +24,22 @@ public:
 	void resize( ResizeEvent event );
 	void update();
 	void draw();
-	
+
 	void togglePacketCapture();
-	void startPacketCapture();
-	void stopPacketCapture();
-	void doPacketCaptureFn();
+
 	
 	
 	gl::Texture				mTexture;
 	gl::GlslProg			mShader;
 	gl::Fbo					mFbo;
-	
-	bool					mPacketCaptureRunning;
-	bool					mPacketCaptureShouldStop;
-	std::thread				mPacketCaptureThread;
 
-	vector<std::string>		mPings;
-	
+	Beacon					mBeacon;
 };
 
 void BeaconPCAPApp::togglePacketCapture()
 {
-	if(mPacketCaptureRunning){
-		stopPacketCapture();
-		console() << "Stopping packet capture." << std::endl;
-	}else{
-		startPacketCapture();
-		console() << "Starting packet capture." << std::endl;
-	}
+	mBeacon.togglePacketCapture();
 }
-void BeaconPCAPApp::startPacketCapture()
-{
-	if(mPacketCaptureRunning) return;
-	try {
-		mPacketCaptureShouldStop = false;
-		mPacketCaptureThread = thread( bind( &BeaconPCAPApp::doPacketCaptureFn, this ) );
-		mPacketCaptureRunning = true;
-	}catch(...){
-		mPacketCaptureRunning = false;
-	}
-	
-}
-
-void BeaconPCAPApp::stopPacketCapture()
-{
-	if(!mPacketCaptureRunning) return;
-	mPacketCaptureShouldStop = true;
-	mPacketCaptureThread.join();
-	mPacketCaptureRunning = false;
-}
-
-void BeaconPCAPApp::doPacketCaptureFn()
-{
-	ci::ThreadSetup threadSetup; // instantiate this if you're talking to Cinder from a secondary thread
-
-	int i;
-	char *dev;
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* descr;
-	const u_char *packet;
-	struct pcap_pkthdr hdr;			/* pcap.h */
-	struct ether_header *eptr;	/* net/ethernet.h */
-	
-	u_char *ptr; /* printing out hardware header info */
-	
-	/* grab a device to peak into... */
-	dev = pcap_lookupdev(errbuf);
-	
-	if(dev == NULL)
-	{
-		printf("%s\n",errbuf);
-		exit(1);
-	}
-	
-	descr = pcap_open_live(dev, BUFSIZ, 1, 10, errbuf);
-	
-	if(descr == NULL)
-	{
-		printf("Error with pcap_open_live(): %s\n", errbuf);
-		exit(1);
-	}
-	
-	while(!mPacketCaptureShouldStop) {
-		packet = pcap_next(descr,&hdr);
-		
-		if(packet == NULL)
-		{
-			// Didn't grab packet
-			continue;
-		}
-		
-		// Get header
-		eptr = (struct ether_header *) packet;
-		
-		// Get packet type (IP, ARP, other)
-		if(ntohs (eptr->ether_type) == ETHERTYPE_IP){
-			//printf("Ethernet type hex:%x dec:%d is an IP packet\n", ntohs(eptr->ether_type), ntohs(eptr->ether_type));
-		}else if(ntohs (eptr->ether_type) == ETHERTYPE_ARP){
-			//printf("Ethernet type hex:%x dec:%d is an ARP packet\n", ntohs(eptr->ether_type), ntohs(eptr->ether_type));
-		}else {
-			//printf("Ethernet type %x not IP", ntohs(eptr->ether_type));
-			continue;
-		}
-		
-		// Get the source address
-		ptr = eptr->ether_shost;
-		i = ETHER_ADDR_LEN;
-		string addy;
-		do {
-			char tmp[10];
-			snprintf(tmp, sizeof(tmp), "%s%x", (i == ETHER_ADDR_LEN) ? "" : ":", *ptr++);
-			addy += tmp;
-			
-		} while(--i > 0);
-		
-		mPings.push_back(addy);
-	}
-	
-}
-
 
 void BeaconPCAPApp::prepareSettings( Settings *settings )
 {
@@ -202,7 +92,8 @@ void BeaconPCAPApp::resize( ResizeEvent event )
 void BeaconPCAPApp::update()
 {
 	// Do something with your texture here.
-	console() << mPings.size() << std::endl;
+
+	console() << mBeacon.getPingCountForMAC(string("10:40:f3:83:a1:9a")) << std::endl;
 }
 
 void BeaconPCAPApp::draw()
